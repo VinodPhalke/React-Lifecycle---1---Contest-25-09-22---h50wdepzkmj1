25b8c6eec2cf7685043fef1e7a4b0622d5e5a2c7
Vinod Phalke <vinodcphalke@gmail.com>
2022-09-25 08:24:08 +0000

    Update package-lock.json and App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,28 @@
-import React from 'react'
+import React from "react";
 
-import '../styles/App.css';
-
+import "../styles/App.css";
 
 class App extends React.Component {
-
-  handleClick(){
-    this.setState({count: this.state.count+1})
+  handleClick() {
+    console.log("rendered");
+    this.setState({ count: this.state.count + 1 });
   }
-  constructor(props){
-    super(props)
-    this.state = {count:0}
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
     this.handleClick = this.handleClick.bind(this);
-
   }
 
   render() {
     return (
       <div id="main">
-          <div id="count">{this.state.count}</div>
-          <button id="incr" onClick={this.handleClick}>Increment</button>
+        <div id="count">{this.state.count}</div>
+        <button id="incr" onClick={this.handleClick}>
+          Increment
+        </button>
       </div>
-    )
+    );
   }
 }
-
 
 export default App;
